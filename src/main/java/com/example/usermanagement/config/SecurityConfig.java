797c70cc0c8b61d83e6b798a214979eeeb593812
sumancfc <sumanstha999@gmail.com>
2026-04-11 11:41:34 +0200

    feat: implement custom authentication entry point for JSON error responses

--- src/main/java/com/example/usermanagement/config/SecurityConfig.java
+++ src/main/java/com/example/usermanagement/config/SecurityConfig.java
@@ -4,12 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomAuthenticationEntryPoint authEntryPoint;
+
+    public SecurityConfig(CustomAuthenticationEntryPoint authEntryPoint) {
+        this.authEntryPoint = authEntryPoint;
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -20,11 +27,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
                 .authorizeHttpRequests(auth -> auth
-                        // This allows everything under /api/v1/users/ to be accessed without a login for now
-                        .requestMatchers("/api/v1/users/**").permitAll()
+                        // Permit only registration and login
+                        .requestMatchers("/api/v1/users/register", "/api/v1/users/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                // Not to create a cookie-bases session
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 }
